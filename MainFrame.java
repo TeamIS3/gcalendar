36580faeeab80f8146a09a3cf9dd040ae81d77b0
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2012-11-08 14:25:44 +0000

    Adds eventless menu items

--- MainFrame.java
+++ MainFrame.java
@@ -23,7 +23,9 @@ public class MainFrame extends JFrame {
     
     private void createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
+        // File menu
         JMenu fileMenu = new JMenu("File");
+        // File -> Exit
         JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -32,6 +34,29 @@ public class MainFrame extends JFrame {
         });
         fileMenu.add(exitItem);
         menuBar.add(fileMenu);
+        
+        // Event menu
+        JMenu eventMenu = new JMenu("Event");
+        // Event -> New event
+        JMenuItem newEventItem = new JMenuItem("New event");
+        eventMenu.add(newEventItem);
+        // Event -> Edit event
+        JMenuItem editEventItem = new JMenuItem("Edit event");
+        eventMenu.add(editEventItem);
+        // Event -> Delete event
+        JMenuItem deleteEventItem = new JMenuItem("Delete event");
+        eventMenu.add(deleteEventItem);
+        // Event menu finished.
+        menuBar.add(eventMenu);
+        
+        // Category menu
+        JMenu categoryMenu = new JMenu("Category");
+        // Category -> View categories
+        JMenuItem viewCategoriesItem = new JMenuItem("View categories");
+        categoryMenu.add(viewCategoriesItem);
+        // Category menu finished.
+        menuBar.add(categoryMenu);
+        
         setJMenuBar(menuBar);
     }
 }
