a69366b820952b3064687c9d9a43351989bc8677
Craig McLaughlin <mr_mcl@live.co.uk>
2012-11-17 22:36:47 +0000

    Added Event class to project.
    Includes a simple test method to display the event information.

---
+++ Event.java
@@ -0,0 +1,60 @@
+
+/**
+ * Stores information regarding an appointment/event.
+ *
+ * @author cmcl
+ * @version 1.0
+ */
+public class Event {
+    private String name;
+    private String location;
+    private String desc; // description
+    private Date startDate;
+    private Date endDate;
+    
+    public Event(String name, String loc, String desc, Date start,
+                 Date end) {
+        this.name = name;
+        location = loc;
+        this.desc = desc;
+        startDate = start;
+        endDate = end;
+    }
+    
+    public Event() {
+        this("", "", "", new Date(), new Date());
+    }
+    
+    public String getName() { return name; }
+    public String getLocation() { return location; }
+    public String getDescription() { return desc; }
+    public Date getStartDate() { return startDate; }
+    public Date getEndDate() { return endDate; }
+    
+    
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        
+        str.append(String.format("Name : "+name+"%n"));
+        str.append(String.format("Location : "+location+"%n"));
+        str.append(String.format("Description : "+desc+"%n"));
+        str.append(String.format("Start Date : "+startDate+"%n"));
+        str.append(String.format("End Date : "+endDate));
+        
+        return str.toString();
+    }
+    
+    /**
+     * Test method
+     *
+     */
+    public static void main(String[] args) {
+        Event e1 = new Event("Partae", "Ma hoose", "Gettin' wrecked",
+                             new Date(31, 12, 2012), new Date(2, 1, 2013));
+        Event e2 = new Event();
+        System.out.println(e1);
+        System.out.println("==============");
+        System.out.println(e2);
+    }
+    
+}
