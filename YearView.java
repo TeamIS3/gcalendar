648c2dcaa828b64b7c85518ba68e382d44caf007
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2012-11-21 21:11:43 +0000

    Fix YearView

--- YearView.java
+++ YearView.java
@@ -12,15 +12,19 @@ public class YearView extends BaseView {
     }
 
     public void setupCalendar() {
-        //add(new JLabel("YEAR VIEW"));
-        String[] names = { "January", "February", "March", "April",
-            "May", "June", "July", "August", "September", "October",
-            "November", "December" };
-        for (int i = 0; i < 12; i++){
+        GridLayout grid = new GridLayout(0, 4);
+        JPanel panel = new JPanel();
+        panel.setLayout(grid);
+        String[] names = { "January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November",
+                "December" };
+        for (int i = 0; i < 12; i++) {
             TableModel temp = new YearDataModel(names[i]);
             JTable month = new JTable(temp);
             JScrollPane scrollPane = new JScrollPane(month);
             this.add(scrollPane, BorderLayout.CENTER);
+            panel.add(scrollPane);
         }
+        this.add(panel);
     }
 }
