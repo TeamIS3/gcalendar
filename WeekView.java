93fe87b6fda927a8521350cef07506e079f1b993
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2012-11-21 21:03:56 +0000

    Tidy up imports for WeekView

--- WeekView.java
+++ WeekView.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
-import javax.swing.table.*;
-
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
  * Week view for the calendar
